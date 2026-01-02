6fed35c56d49ecbc4d9f3ee670cd6f3c8fe30084
Izzexy <103591122+Izzexy@users.noreply.github.com>
2026-01-02 15:44:58 +0100

    Log current time and update message sending
    Added time logging and modified message sending logic.

--- client/main.c
+++ client/main.c
@@ -2,14 +2,22 @@
 #include "../lib/libmq.h"
 #include <stdio.h>
 
+#include <time.h>
+
 int main(int argc, char **argv) {
   char *addr = "127.0.0.1";
   char *port = "7654";
 
+  printf("time: %ld\n", time(NULL));
+
   mqClient *client = NULL;
   mqClientInit(&client, addr, port);
   mqClientCreate(client, mqCStr("topic3"));
-  mqClientSend(client, mqCStr("topic3"), mqCStr("Hello, world"), 1798717378);
+  //mqClientSend(client, mqCStr("topic3"), mqCStr("Hello, world"), 1898717378);
+  mqClientJoin(client, mqCStr("topic3"));
+  //mqClientSend(client, mqCStr("topic3"), mqCStr("Hello, world 22"), 1898717378);
+  mqClientSend(client, mqCStr("topic3"), mqCStr("Hello, world 44"), 1898717378);
+
   for (;;) {
     mqMsg *msg = NULL;
     // mqClientRecv(client, &msg);
